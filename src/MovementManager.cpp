166d6bbbd7dc7f77b499b31ae9e995b510d84a4f
Jacob <75269577+Triadziuch@users.noreply.github.com>
2024-02-14 16:25:57 +0100

    Code clean-up

--- src/MovementManager.cpp
+++ src/MovementManager.cpp
@@ -9,3 +9,8 @@ MovementManager::MovementManager()
 	assert(this->sInstance == nullptr);
 	sInstance = this;
 }
+
+const MovementRoutine* MovementManager::createMovementRoutine(const sf::Shape* _shape, const std::string& _name)
+{
+	return nullptr;
+}

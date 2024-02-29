3495a435903ff2d5870e1492fd72862584e3a8fa
Jacob <75269577+Triadziuch@users.noreply.github.com>
2024-02-29 22:25:28 +0100

    Overrided clear, resize and append functions for VertexArray2

--- src/VertexArray2.cpp
+++ src/VertexArray2.cpp
@@ -3,7 +3,6 @@
 
 void VertexArray2::updateScale()
 {
-
 	for (size_t i = 0; i < _vertexarray->getVertexCount(); i++) {
 		_vertexarray->operator[](i).position.x = _scalingInfo->centroid.x + (_scalingInfo->originalVertex.operator[](i).position.x - _scalingInfo->centroid.x) * _scalingInfo->current_scale.x;
 		_vertexarray->operator[](i).position.y = _scalingInfo->centroid.y + (_scalingInfo->originalVertex.operator[](i).position.y - _scalingInfo->centroid.y) * _scalingInfo->current_scale.y;
@@ -18,9 +17,40 @@ void VertexArray2::setPosition(const sf::Vector2f& position)
 {
 }
 
+sf::Vertex& VertexArray2::operator [](std::size_t index)
+{
+	m_need_update = true;
+	return sf::VertexArray::operator[](index);
+}
+
 void VertexArray2::setScale(float factorX, float factorY)
 {
 	m_scale.x = factorX;
 	m_scale.y = factorY;
 	updateScale();
+}
+
+void VertexArray2::clear()
+{
+	sf::VertexArray::clear();
+	m_original_scaling.clear();
+	m_original_rotation.clear();
+}
+
+void VertexArray2::resize(std::size_t vertexCount)
+{
+	sf::VertexArray::resize(vertexCount);
+	m_original_scaling.resize(vertexCount);
+	m_original_rotation.resize(vertexCount);
+}
+
+void VertexArray2::append(const sf::Vertex& vertex)
+{
+	sf::VertexArray::append(vertex);
+	m_original_scaling.push_back(vertex);
+	m_original_rotation.push_back(vertex);
+}
+
+void VertexArray2::setOrigin(float x, float y)
+{
 }
--- src/VertexArray2.h
+++ src/VertexArray2.h
@@ -6,18 +6,26 @@ private:
 	sf::Vector2f	m_origin;
 	sf::Vector2f	m_scale;
 	float			m_rotation;
+	bool			m_need_update;
 
-	sf::VertexArray m_original_scaling;
-	sf::VertexArray m_original_rotation;
+	std::vector<sf::Vertex> m_original_scaling;
+	std::vector<sf::Vertex> m_original_rotation;
 
 	void updateScale();
 
 public:
 
 	VertexArray2() : sf::VertexArray{}, m_origin{}, m_scale{ 1, 1 }, m_rotation{ 0 } {}
-	VertexArray2(sf::PrimitiveType type, size_t vertexCount) : sf::VertexArray{type, vertexCount}, m_origin{}, m_scale{1, 1}, m_rotation{0} {}
-	VertexArray2(const VertexArray2& other) : sf::VertexArray{other}, m_origin{other.m_origin}, m_scale{other.m_scale}, m_rotation{other.m_rotation} {}
+	VertexArray2(sf::PrimitiveType type, size_t vertexCount = 0) : sf::VertexArray{type, vertexCount}, m_origin{}, m_scale{1, 1}, m_rotation{0}, m_original_scaling(vertexCount), m_original_rotation(vertexCount) {}
+	VertexArray2(const VertexArray2& other) : sf::VertexArray{ other }, m_origin{ other.m_origin }, m_scale{ other.m_scale }, m_rotation{ other.m_rotation }, m_need_update{ true } {}
 	~VertexArray2() {};
+
+	void clear();
+	void resize(std::size_t vertexCount);
+	void append(const sf::Vertex& vertex);
+
+	sf::Vertex& operator [](std::size_t index);
+	const sf::Vertex& operator [](std::size_t index) const;
 
 	operator sf::VertexArray& () { return *this; }
 	operator const sf::VertexArray& () const { return *this; }
